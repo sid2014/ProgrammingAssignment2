53f6b7fad8d2a2f617025e6558135c323c488919
sid2014 <siddardh.siddu@gmail.com>
2016-09-28 22:09:02 +0530

    Updated cachematrix.R with my coursera assignment
    This file will have two functions, in which one calculates the list of matrix and its inverse and the other function uses the cache whenever possible or cache the output if it is the first time we are making that calculation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below functions will prevent calculating the inverse of a same matrix over and over again if it was passed through arguments
+## This will be achieved, checking if cache exists before calculating 
+## If not, caching the calculated value everytime once the calculation is done
 
-## Write a short comment describing this function
+## The below function will prepare a list of input matrix and its cached inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(ip_matrix = matrix()) {
+        
+ # Check if the input is a matrix or not !      
+        
+        if(class(ip_matrix) != "matrix") 
+        { stop("This is only for matrices") }
+        
+        dimensions <- dim(ip_matrix)
+        
+ # Check if solve function can be applied on the input matrix 
+        
+        if( dimensions[1] != dimensions[2] )
+        {
+                stop(" General inverse is not possible with this matrix ")
+        }
+        
+        cache_matrix <- NULL
+        get_matrix <- function() ip_matrix
+        set_matrix_inverse <- function(cache_inverse) cache_matrix <<- cache_inverse
+        get_matrix_inverse <- function() cache_matrix
 
+# Prepre input to the cacheSolve function    
+                
+        list(get_matrix = get_matrix,
+             set_matrix_inverse = set_matrix_inverse,
+             get_matrix_inverse = get_matrix_inverse)
 }
 
+## The below function will reuse the cache value if possible. If not, will cache the calculated value for next use 
+                
+cacheSolve <- function(makeCacheMatrix_op) {
+        
+        cache_matrix <- makeCacheMatrix_op$get_matrix_inverse()
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Check if cache exists or not        
+        
+        if(!is.null(cache_matrix)) 
+        {
+                message("getting cached data")
+                return(cache_matrix)
+        }
+# If no cache data found, then pick the matrix from the input list and apply sove function
+        data <- makeCacheMatrix_op$get_matrix()
+        cache_matrix <- solve(data)
+        
+# Cache the calcuated value, so that we can pick it up if the same input is passed again        
+        
+        makeCacheMatrix_op$set_matrix_inverse(cache_matrix)
+        cache_matrix
 }
